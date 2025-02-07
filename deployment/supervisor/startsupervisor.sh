308fbc92cd49469e0d75fe283719bf8fee545d4d
Nuraaly <dzhuzupbekovnuraaly@gmail.com>
2025-02-07 14:08:31 +0600

    update deployment initial

--- deployment/supervisor/startsupervisor.sh
+++
@@ -1,9 +0,0 @@
-echo "DOCKER_ENABLE_SUPERVISOR=$RUN_SUPERVISOR"
-
-if [ "$RUN_SUPERVISOR" = "true" ]; then
-    echo "Starting Supervisor..."
-    wait 60
-    supervisord -n
-else
-    echo "Supervisor is disabled"
-fi
